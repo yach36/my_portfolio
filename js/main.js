7a09f23075cb6f886d01db4b1a2213974a1e2174
yach36 <yach0306@gmail.com>
2022-07-06 02:41:05 +0900

    1ページスクロールを実装しました

---
+++ js/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+$(function() {
+  /* ---------- 変数 ---------- */
+  let scrollTime = 500;
+  /* ---------- 関数 ---------- */
+
+  /* ---------- scrollify ---------- */
+  $.scrollify({
+    section: ".top_section",
+    easing: "swing",
+    scrollSpeed: scrollTime,
+    after: function() {
+      // 現在のidの末尾の番号を取得
+      let num = Number($.scrollify.current()[0]["id"].slice(-1));
+      /* スクロールゲージの色を変える */
+      $(".js_scroll_gauge").not(`#js_scroll_gauge-${num}`).removeClass("is_active");
+      $(`#js_scroll_gauge-${num}`).addClass("is_active");
+    }
+  });
+});
